1afe0fc994f08a3982f95755c2ecdfc853395a15
X-root255 <asifgikian@yahoo.com>
2017-02-08 12:45:46 +0000

    Some changes to the code
    1. Check if method exists in the class
    2. link currently visiting in browser added to the ErrorController
    3, BASE_URL constant adde to config/app.php file

--- app/Codecourse/Bootstrap/Bootstrap.php
+++ app/Codecourse/Bootstrap/Bootstrap.php
@@ -15,6 +15,7 @@ class Bootstrap
 		$ca = rtrim($ca,'/');
 
 		$ca = explode('/', $ca);
+
 		$actionName = DEFAULT_ACTION;
 		if ($ca[0] != '')
 		{
@@ -27,13 +28,22 @@ class Bootstrap
 		else
 		{
 			$controllerName = DEFAULT_CONTROLLER_NAMESPACE . DEFAULT_CONTROLLER;
-		}	
+		}
+		
 		if ( ! class_exists($controllerName) )
 		{
 			$controllerName = DEFAULT_CONTROLLER_NAMESPACE . ERROR_CONTROLLER;
 			$actionName = ERROR_NOT_FOUND_ACTION;
 		}
+		
 		$controller = new $controllerName();
+		
+		if ( ! method_exists($controller, $actionName) )
+		{
+			$controllerName = DEFAULT_CONTROLLER_NAMESPACE . ERROR_CONTROLLER;
+			$actionName = ERROR_NOT_FOUND_ACTION;
+		}
+
 		$controller->{$actionName}();
 	}
 }
--- app/Codecourse/Controllers/ErrorController.php
+++ app/Codecourse/Controllers/ErrorController.php
@@ -8,13 +8,14 @@ class ErrorController extends Controller
 	
 	public function errorNotFound()
 	{
+		$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 		echo '
 			<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
 			<html><head>
 			<title>404 Not Found</title>
 			</head><body>
 			<h1>Not Found</h1>
-			<p>The requested URL /m.akber/ddd was not found on this server.</p>
+			<p>The requested URL ' . $actual_link . ' was not found on this server.</p>
 			<hr>
 			<address>Apache/2.4.23 (Win64) PHP/5.6.25 Server at localhost Port 80</address>
 			</body></html>
